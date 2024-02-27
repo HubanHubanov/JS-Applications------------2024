ab18b80f1976250b7a2b48a8d3f8081c19e067b9
HubanHubanov <huban.hubanov@gmail.com>
2024-02-27 16:03:19 +0200

    add 01 BusStop and resources

---
+++ 03.Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -0,0 +1,55 @@
+async function getInfo() {
+
+
+    const stopIdRef = document.getElementById("stopId")
+    const stopId = stopIdRef.value
+
+    const stopNameRef = document.getElementById("stopName")
+    const busesRef = document.getElementById("buses")
+
+    stopIdRef.textContent = ""
+    busesRef.innerHTML = "  "
+
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
+
+    try {
+        const response = await fetch(url)
+    const data = await response.json()
+    stopNameRef.textContent = data.name
+    appendChild(Object.entries(data.buses))
+        
+    } catch (error) {
+                stopNameRef.textContent = "Error"
+
+        
+    }
+
+
+    
+
+    // const response = fetch(url)
+    // response.then(res => {
+    //     res.json().then(data=> {
+      
+    //      stopNameRef.textContent = data.name
+    //      appendChild(Object.entries(data.buses))
+         
+     
+    //     }).catch(err => {
+    //         stopNameRef.textContent = "Error"
+    //     })
+    // }).catch(err =>{
+    //     stopNameRef.textContent = "Error"
+        
+    // })
+
+    function appendChild(data) {
+        for([bus,time] of data) {
+            let li = document.createElement("li")
+            li.textContent = `Bus ${bus} arrives in ${time} minutes`
+            busesRef.appendChild(li)
+
+        }
+    }
+   
+}   
