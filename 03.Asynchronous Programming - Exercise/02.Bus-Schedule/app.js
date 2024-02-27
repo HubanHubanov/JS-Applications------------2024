0f742f0d652f9cfe9bbb396854d789e3663823d3
HubanHubanov <huban.hubanov@gmail.com>
2024-02-27 16:55:00 +0200

    add 02.BusSchedule

--- 03.Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ 03.Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,46 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const infoSpanRef = document.querySelector("#info span");
+    const departBtnRef = document.getElementById("depart");
+    const arriveBtnRef = document.getElementById("arrive");
+
+    const url = "http://localhost:3030/jsonstore/bus/schedule/"
+
+    const stop = {
+        currentStop: "",
+        nextStop: "depot"
+    }
+
+   async function depart() {
+
+    try {
+        const response = await fetch(url + stop.nextStop )
+        const data = await response.json()
+        stop.currentStop = data.name;
+        stop.nextStop = data.next
+        infoSpanRef.textContent = `Next stop ${stop.currentStop}`
+ 
+        departBtnRef.disabled = true
+        arriveBtnRef.disabled = false
+    } catch (error) {
+        infoSpanRef.textContent = "Error"
+        
+        departBtnRef.disabled = true
+        arriveBtnRef.disabled = true
+    }
+
+      
+    
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoSpanRef.textContent = `Arriving at ${stop.currentStop}`
+
+        departBtnRef.disabled = false
+        arriveBtnRef.disabled = true
+     
+
+    
     }
 
     return {
