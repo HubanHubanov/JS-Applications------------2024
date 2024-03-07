2532d8d5f94ca38cd469db7f2bec2531d5a40333
HubanHubanov <huban.hubanov@gmail.com>
2024-03-07 19:59:12 +0200

    update

---
+++ 07.Single Page Applications - Exercise/02.Movies/src/app.js
@@ -0,0 +1,16 @@
+// [ ] improve HTML structure
+// [ ] create app.js module
+// [ ] create router.js containing hide and display of view
+// [ ] placeholders for all view
+
+// implement views
+// -create request logic
+// - DOM manipulation logic
+// [ ] catalog
+// [ ] login
+// [ ] register
+// [ ] details
+// [ ] edit
+// [ ] delete
+// [ ] like
+
