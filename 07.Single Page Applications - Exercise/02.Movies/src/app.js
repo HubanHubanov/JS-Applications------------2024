2cabb069169358267eaa5be7ecf7d8dfd7ae050a
HubanHubanov <huban.hubanov@gmail.com>
2024-03-13 13:05:04 +0200

    add resources

--- 07.Single Page Applications - Exercise/02.Movies/src/app.js
+++ 07.Single Page Applications - Exercise/02.Movies/src/app.js
@@ -1,16 +1,71 @@
 // [ ] improve HTML structure
 // [ ] create app.js module
-// [ ] create router.js containing hide and display of view
-// [ ] placeholders for all view
+// [ ] create util.js containing hode and display view
+// [ ] placeholders for all views
 
 // implement views
-// -create request logic
+// - create request 
 // - DOM manipulation logic
+
 // [ ] catalog
-// [ ] login
+// [ ] login 
 // [ ] register
+// [ ] create
 // [ ] details
+// [ ] like
 // [ ] edit
 // [ ] delete
-// [ ] like
+
+
+import { homePage } from "./home.js";
+import { loginPage } from "./login.js"
+import { registerPage } from "./register.js";
+import { createPage } from "./create.js";
+import { updateNav } from "./util.js";
+
+
+// showView("#home-page");
+
+const routes = {
+    "/": homePage,
+    "/login": loginPage,
+    "/logout": logout,
+    "/register": registerPage,
+    "/create":createPage,
+
+}
+
+
+document.querySelector("nav").addEventListener("click", onNavigate);
+document.querySelector("#add-movie-button a").addEventListener("click", onNavigate)
+
+function onNavigate(event) {
+    if(event.target.tagName == "A" && event.target.href) {
+        event.preventDefault();
+         const url = new URL(event.target.href);
+     
+    const view = routes[url.pathname];
+    
+    if(typeof view == "function") {
+        view()
+    }
+   
+    }   
+}
+
+
+
+
+function logout() {
+  localStorage.removeItem("user");
+  updateNav()
+}
+
+// start application in catalog view
+updateNav
+homePage()
+
+
+
+
 
---
+++ 09.Architecture and Testing - Exercise/03.SoftTerest/app.js
@@ -0,0 +1,2 @@
+// document.querySelectorAll("div[data-selection='section']").forEach(div => div.remove())
+console.log("?????/");
