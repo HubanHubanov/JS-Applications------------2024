0d628642fabc8aeece758f826308594ac8c00829
HubanHubanov <huban.hubanov@gmail.com>
2024-03-17 13:40:01 +0200

    draft

--- 11.Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown.js
+++ 11.Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,39 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+const root = document.getElementById("menu")
+const url ="http://localhost:3030/jsonstore/advanced/dropdown";
+document.querySelector("form").addEventListener("submit", addItem);
+
+
+onLoad()
+async function onLoad() {
+   const response = await fetch(url);
+   const data = await response.json()
+   const option = Object.values(data).map(op => optionTemplate(op));
+   update(option)
+}
+
+function optionTemplate(data) {
+     return html`<option values=${data._id}>${data.text}</option>`
+     /// value ? vaues
+}
+
+function update(data) {
+   render(data, root)
+}
+
+function addItem(e) {
+    e.preventDefault();
+    const text = document.getElementById("itemText").value;
+    addItemInDb(text)
+}
+
+async function addItemInDb(data) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+         body: JSON.stringify(data)
+    })
+    onLoad()
 }
