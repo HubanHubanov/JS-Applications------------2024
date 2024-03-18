ba6e990045e5ed56dcec74a49b8df8c143df069a
HubanHubanov <huban.hubanov@gmail.com>
2024-03-18 15:04:53 +0200

    add MoviesSPADraft

---
+++ 12.Routing/02.Movies/src/app.js
@@ -0,0 +1,8 @@
+import { page }  from "./lib.js";
+
+import { showHome } from "./views/home.js";
+
+page("/", showHome);
+
+page.start()
+
---
+++ 12.Routing/02.Movies/src/util.js
@@ -0,0 +1,12 @@
+export function saveUserData(data) {
+       localStorage.setItem("user", JSON.stringify(data))
+}
+
+export function getUserData() {
+  return JSON.parse(localStorage.getItem("user"))
+}
+
+export function clearUserData() {
+
+    localStorage.removeItem("user")
+}
